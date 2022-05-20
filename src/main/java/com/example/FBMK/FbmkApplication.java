ec0c52b513dce3bfe8305825d8e60f35047be817
othmanekn <othman.elkinany2@gmail.com>
2022-05-20 16:35:44 +0200

    initial commit

---
+++ src/main/java/com/example/FBMK/FbmkApplication.java
@@ -0,0 +1,13 @@
+package com.example.FBMK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbmkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbmkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FBMK/FbmkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FBMK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FbmkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

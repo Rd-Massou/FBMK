68e53bf7db668a7233305d196024ee2ca90f86ab
othmanekn <othman.elkinany2@gmail.com>
2022-05-20 16:49:41 +0200

    setting up project

---
+++ src/main/java/com/example/FBMK/CalculatorController.java
@@ -0,0 +1,18 @@
+package com.example.FBMK;
+
+import com.example.FBMK.models.AdditionRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calculator")
+public class CalculatorController {
+
+
+    @PostMapping("/add")
+    public Float add(@RequestBody AdditionRequest body){
+        return  1.0f;
+    }
+}
---
+++ src/main/java/com/example/FBMK/models/AdditionRequest.java
@@ -0,0 +1,12 @@
+package com.example.FBMK.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AdditionRequest {
+
+    private Float a;
+    private Float b;
+}
